d5406e6ab0cf5ec742524c67db680724cdf4f094
Yrocky <983272765@qq.com>
2018-07-22 19:19:19 +0800

    Initial Commit

---
+++ MMLoger/MMLoger.h
@@ -0,0 +1,19 @@
+//
+//  MMLoger.h
+//  MMLoger
+//
+//  Created by Rocky Young on 2018/7/22.
+//  Copyright © 2018年 Rocky Young. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MMLoger.
+FOUNDATION_EXPORT double MMLogerVersionNumber;
+
+//! Project version string for MMLoger.
+FOUNDATION_EXPORT const unsigned char MMLogerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MMLoger/PublicHeader.h>
+
+
---
+++ MMLogerTests/MMLogerTests.m
@@ -0,0 +1,39 @@
+//
+//  MMLogerTests.m
+//  MMLogerTests
+//
+//  Created by Rocky Young on 2018/7/22.
+//  Copyright © 2018年 Rocky Young. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MMLogerTests : XCTestCase
+
+@end
+
+@implementation MMLogerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
